2fa64ad3fbe554230b6419ce04f7badd3ac6a7b2
Kyle Kelley <rgbkrk@gmail.com>
2015-06-30 01:07:18 -0500

    Simple shell send
    (non working commit, gotta go to bed)

--- main.js
+++ main.js
@@ -40,9 +40,6 @@ function launchSideCar(session) {
     session.iopubSocket.on("message", function (message) {
       sideCar.webContents.send("message", message);
     });
-    session.shellSocket.on("message", function(message){
-      console.log(message);
-    });
   });
 
   // Emitted when the window is closed.
